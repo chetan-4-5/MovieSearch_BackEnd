a6f3c4eb68d0b3b67679d0da82eb98dd6a63b4d6
Chetan Reddy <sharmila.chetan.2003@gmail.com>
2024-06-13 14:51:02 +0530

    update 2

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,22 @@
 const express = require('express');
 const { registerUser, authUser } = require('../controllers/userController');
 const router = express.Router();
+const { getUserById } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 
+router.get('/user', protect, getUserById);
+module.exports = router;
+const asyncHandler = require('express-async-handler');
+const User = require('../models/User');
+exports.getUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id).select('-password');
+    if (user) {
+        res.json({ user });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+});
 
 router.post('/register', registerUser);
 router.post('/login', authUser);
