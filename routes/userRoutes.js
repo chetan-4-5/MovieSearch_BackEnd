9772af06aa9f78830626e98ede338b2450fd0fee
Chetan Reddy <sharmila.chetan.2003@gmail.com>
2024-06-13 15:56:54 +0530

    update 2

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,49 @@
-// routes/userRoutes.js
-const express = require('express');
-const { registerUser, authUser } = require('../controllers/userController');
-const router = express.Router();
-const authMiddleware = require('../middleware/auth');
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 
-router.post('/register', registerUser);
-router.post('/login', authUser);
-// GET current user
-router.get('/user', authMiddleware, async (req, res) => {
-    try {
-        // Fetch user data based on authenticated user (req.user.id)
-        const user = req.user.id;
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        res.json(user); // Return user data
-    } catch (error) {
-        console.error('Error fetching current user:', error);
-        res.status(500).json({ message: 'Server error' });
-    }
-});
+const AuthContext = createContext();
 
-module.exports = router;
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        const fetchCurrentUser = async () => {
+            const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+            console.log('AuthContext: Token:', token);
+
+            if (token) {
+                try {
+                    const response = await axios.get('https://moviesearch-backend-b97z.onrender.com/api/user', {
+                        headers: { 'x-auth-token': token }
+                    });
+
+                    console.log('AuthContext: API Response Status:', response.status);
+                    console.log('AuthContext: API Response Data:', response.data);
+
+                    if (response.status === 200) {
+                        setCurrentUser(response.data);
+                        console.log('AuthContext: Current User:', response.data);
+                    } else {
+                        console.error('AuthContext: Failed to fetch user. Status:', response.status);
+                        setCurrentUser(null);
+                    }
+                } catch (error) {
+                    console.error('AuthContext: Error fetching current user:', error);
+                    setCurrentUser(null);
+                }
+            } else {
+                setCurrentUser(null);
+            }
+        };
+
+        fetchCurrentUser();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ currentUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
